0e52c2ef98418a68387a3a4b89f4ca452ecc1c34
Alexandre de Cássio Rodrigues <89478921+alexandrerodrigues2311@users.noreply.github.com>
2026-07-17 07:10:18 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -203,7 +203,7 @@ export default function App() {
         await fetch(GOOGLE_SCRIPT_URL, {
           method: "POST",
           mode: "no-cors",
-          headers: { "Content-Type": "application/json" },
+          headers: { "Content-Type": "text/plain;charset=utf-8" },
           body: JSON.stringify(dataToSave),
         });
       }
